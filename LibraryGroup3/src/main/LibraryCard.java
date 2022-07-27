33c9c81e8df059ce73a454d7314f65cf087652d8
Gunnar Moffitt <Gjm54@txstate.edu>
2022-07-26 21:40:39 -0500

    starting the skeleton for USER

---
+++ LibraryGroup3/src/main/LibraryCard.java
@@ -0,0 +1,20 @@
+package main;
+
+public class LibraryCard {
+	
+	private int cardNumber;
+	
+	public LibraryCard(int a) {
+		setCardNumber(a);
+	}
+	
+	
+	
+	public void setCardNumber(int a) {
+		cardNumber = a;
+	};
+	
+	public int getCardNumber() {
+		return cardNumber;
+	};
+}
---
+++ LibraryGroup3/src/main/User.java
@@ -0,0 +1,15 @@
+package main;
+
+public class User {
+	
+	private String name;
+	private String address;
+	private String phoneNumber;
+	
+	private LibraryCard ID;
+	
+	
+			
+	
+	
+}
