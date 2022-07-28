1b329d21538d301280faa4ac5df015827970ea9c
Gage <gld37@txstate.edu>
2022-07-27 22:55:31 -0500

    Library.java
    first push, still learning to use git software

---
+++ LibraryGroup3/src/main/Library.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Libary {
+
+    book;
+
+    AudioOrVideo;
+
+    Magazine;
+
+    RefererenceBook;
+
+    Journal;
+
+    ArrayList<Users> user;
+
+    public void getCheckedOut(User user){}
+
+    public void getFines(User user){}
+
+    public void addUser(){}
+
+    public void getUser(){}
+
+    public void getDueDate(item){}
+
+}
