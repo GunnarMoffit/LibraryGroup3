881dcf1112b0644b6c85abccb34bf0fa05aedb1f
Gage <gld37@txstate.edu>
2022-07-29 16:00:00 -0500

    Added to library class
    Still need a way to get users specific item info, I made a fake method User.getItems()

--- LibraryGroup3/src/main/Library.java
+++ LibraryGroup3/src/main/Library.java
@@ -1,27 +1,123 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Libary {
 
-    book;
+    //need method to return user specific checked out items "User.getItems()"
+    //need method to return item specific fines, Item.getFine()
+    //need method to return item specific due date, Item.getDueDate()
+    //need user constructor method in user class
 
-    AudioOrVideo;
+    //Map containing Library Card #'s as keys, and User objects as values
+    //HashMap<Integer, User> usersMap = new HashMap<>();
+    //
+    //Map containing Item names as keys, and Item objects as values
+    //HashMap<String, Item> itemMap = new HashMap<>();
 
-    Magazine;
+    //List of Item objects, can be book, magazine, etc..
+    ArrayList<Item> items = new ArrayList<>();
 
-    ReferenceBook;
+    //List of User objects, has name, address, etc..
+    ArrayList<User> users = new ArrayList<>();
 
-    Journal;
+    public void addUser(String name, String address, int phonenumber, int libraryCardID){
 
-    ArrayList<Users> user;
+        User newUser = new User(name, address, phonenumber, libraryCardID);
 
-    public void getCheckedOut(User user){}
+        users.add(newUser);
 
-    public void getFines(User user){}
+    }
 
-    public void addUser(){}
+    public void addItem(String name, Float value, boolean loanable, boolean restricted, boolean available){
 
-    public void getUser(){}
+        Item newItem = new Item(name, value, loanable, restricted, available);
 
-    public void getDueDate(item){}
+        items.add(newItem);
+    }
+
+    public ArrayList<User> getUserList(){
+        return users;
+    }
+
+    public ArrayList<Items> getItemList(){
+        return items;
+    }
+
+    public void getUserInfo(int libraryCardNum){
+
+        for(User : users) {
+            if(User.getCardNumber() == libraryCardNum){
+                System.out.println("User found.\n");
+                System.out.println("Name: ")
+                System.out.println(User.getName() + "\n");
+                System.out.println("Address: ")
+                System.out.println(User.getAddress() + "\n");
+                System.out.println("Phone Number: ")
+                System.out.println(User.getPhoneNumber() + "\n");
+                System.out.println("Library Card Number: ")
+                System.out.println(libraryCardNum + "\n");
+            }
+            else {
+                System.out.println("User not found. Please enter a valid library card number.");
+            }
+        }
+    }
+
+    public void getItemInfo(String name){
+
+        for(Item : items) {
+            if(Item.getName() == name){
+                System.out.println("Item found.\n");
+                System.out.println("Name: ")
+                System.out.println(Item.getName() + "\n");
+                System.out.println("Value: ")
+                System.out.println(Item.getValue() + "\n");
+                System.out.println("Loanability: ")
+                System.out.println(Item.isLoanable() + "\n");
+                System.out.println("Restriction: ")
+                System.out.println(Item.isRestricted + "\n");
+                System.out.println("Availability: ");
+                System.out.println(Item.isAvailable + "\n");
+            }
+            else {
+                System.out.println("Item not found. Please enter a valid item name.");
+            }
+        }
+    }
+
+    public void getUserCheckedOutItems(int libraryCardNum){
+        for(User : users) {
+            if(User.getCardNumber() == libraryCardNum){
+                System.out.println("User: " + User + "\n" + "Items: ");
+                for(Item : User.getItems()) {
+                    System.out.println(Item.getName() + "\n");
+                }
+            }
+        }
+    }
+
+    public void getUserItemsDueDates(int LibraryCardNum){
+        for(User : users) {
+            if(User.getCardNumber() == libraryCardNum){
+                System.out.println("User: " + User + "\n" + "Items with due date: ");
+                for(Item : User.getItems()) {
+                    System.out.println(Item.getName() + ": " + Item.getDueDate() + "\n");
+                }
+            }
+        }
+    }
+
+    public void getUserFines(int LibraryCardNum){
+        for(User : users) {
+            if(User.getCardNumber() == libraryCardNum){
+                System.out.println("User: " + User + "\n" + "Item fines: ");
+                for(Item : User.getItems()) {
+                    System.out.println(Item.getName() + ": " + Item.getFine() + "\n");
+                }
+            }
+        }
+    }
 
 }
